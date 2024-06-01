3660fbcb7e53abbab5f679f535bd6bcb2b77e15c
barckcode <cristian@helmcode.com>
2024-06-01 21:22:07 +0100

    Changed DB Engine

--- api/db/config.py
+++ api/db/config.py
@@ -1,9 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+import os
 
 
-engine = create_engine('sqlite:///puyu.db')
+database_connection_string = os.getenv('DATABASE_CONNECTION_STRING')
+
+engine = create_engine(f'postgresql+psycopg2://{database_connection_string}')
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
